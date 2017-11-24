8bfb3a55a69da8597f9d409e2e4875f1ed728a9e
mahmudka <mahmudka@mail.ru>
2017-11-24 09:36:13 +0200

    Исправления ошибок #2

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,25 @@
 
 'use strict';
 
+var getMaxElement = function (times) {
+  var max = -1;
+  for (var i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+  return max;
+};
+
+var setColor = function (ctx, names) {
+  if (names === 'Вы') {
+    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  } else {
+    ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random() + ')';
+  }
+}
+
 window.renderStatistics = function (ctx, names, times) {
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
@@ -10,19 +29,11 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(100, 10, 420, 270);
 
 
-  ctx.fillStyle = '#000'; // black;
+  ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 160, 25);
   ctx.fillText('Список результатов:', 160, 40);
-
-  var max = -1;
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }
 
   var gistogramParams = {
     itemWidth: 40,
@@ -33,26 +44,15 @@ window.renderStatistics = function (ctx, names, times) {
   };
 
   var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
+  var step = histogramHeight / (getMaxElement(times) - 0);
 
-  // var itemWidth = 40;
-  // var indent = 50;
-  // var initialX = 120;
-  // var initialY = 260;
-  // var lineBottom = 15;
-
-  for (i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 153, ' + Math.random() + ')';
-    }
+  for (var i = 0; i < times.length; i++) {
+    setColor(ctx, names[i]);
     ctx.fillRect(gistogramParams.initialX + 1.5 * gistogramParams.indent * i + gistogramParams.itemWidth,
         gistogramParams.initialY, gistogramParams.itemWidth, times[i] * step - gistogramParams.initialY);
     ctx.fillText(names[i], gistogramParams.initialX + 1.5 * gistogramParams.indent * i + gistogramParams.itemWidth,
         times[i] * step - gistogramParams.lineBottom);
-    ctx.fillText(times[i].toFixed(2), gistogramParams.initialX + 1.5 * gistogramParams.indent * i + gistogramParams.itemWidth,
-        gistogramParams.initialY + gistogramParams.lineBottom);
+    ctx.fillText(times[i].toFixed(0), gistogramParams.initialX + 1.5 * gistogramParams.indent * i + gistogramParams.itemWidth, gistogramParams.initialY + gistogramParams.lineBottom);
   }
 };
 
