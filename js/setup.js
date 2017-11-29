795fd76ddf902762cc2c94b13f76fc673b244bab
mahmudka <mahmudka@mail.ru>
2017-11-29 12:27:58 +0200

    #4 Код и Магия

--- js/setup.js
+++ js/setup.js
@@ -5,9 +5,11 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COATCOLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYESCOLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIRECOLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+// userDialog.classList.remove('hidden');
 
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
@@ -20,7 +22,8 @@ for (var i = 0; i < 4; i++) {
   wizards.push({
     name: WIZARD_NAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))] + '\n' + WIZARD_LASTNAMES[Math.floor(Math.random() * (WIZARD_NAMES.length))],
     coatColor: WIZARD_COATCOLORS[Math.floor(Math.random() * (WIZARD_NAMES.length))],
-    eyesColor: WIZARD_EYESCOLORS[Math.floor(Math.random() * (WIZARD_NAMES.length))]
+    eyesColor: WIZARD_EYESCOLORS[Math.floor(Math.random() * (WIZARD_NAMES.length))],
+    fireColor: WIZARD_FIRECOLORS[Math.floor(Math.random() * (WIZARD_FIRECOLORS.length))]
   });
 }
 
@@ -43,3 +46,92 @@ similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
 
+// Открытие и закрытие popup-ов
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+var setupUserName = setup.querySelector('.setup-user-name');
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireBall = document.querySelector('.setup-fireball-wrap');
+
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = wizards[Math.floor(Math.random() * (WIZARD_NAMES.length)) - 1].coatColor;
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = wizards[Math.floor(Math.random() * (WIZARD_NAMES.length)) - 1].eyesColor;
+});
+
+wizardFireBall.addEventListener('click', function () {
+  wizardFireBall.style.backgroundColor = wizards[Math.floor(Math.random() * (WIZARD_FIRECOLORS.length)) - 1].fireColor;
+});
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+setupUserName.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    evt.preventDefault();
+  }
+});
+
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
